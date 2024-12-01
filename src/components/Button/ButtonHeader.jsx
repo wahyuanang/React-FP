456442d194ac989b7ca41b9f4724615170ce88f8
wahyuanang <wahyuanng@students.amikom.ac.id>
2024-12-02 01:37:00 +0700

    feat: add modal selection flightnumbers

--- src/components/Button/ButtonHeader.jsx
+++ src/components/Button/ButtonHeader.jsx
@@ -1,23 +1,49 @@
-import React from 'react'
-import {ArrowLeft, Filter, Search } from 'lucide-react'
+import React, { useState } from 'react';
+import { ArrowLeft, Filter, Search } from 'lucide-react';
+import SearchFlightModal from '../Modals/SearchFlightModal'; 
 
 const ButtonHeader = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false); 
+  const [selectedFlightNumber, setSelectedFlightNumber] = useState("");
+
+  const handleFlightNumberSelect = (flightNumber) => {
+    setSelectedFlightNumber(flightNumber);
+    setIsModalOpen(false);
+  };
+
+  const handleSearchClick = () => {
+    setIsModalOpen(true); 
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="mx-auto px-4 sm:px-6 lg:px-8 max-w-screen-lg flex items-center space-x-3">
-    <button className='flex gap-8 bg-[#A06ECE] text-white rounded-[12px] p-5 w-full'>
-        <ArrowLeft/>
+      <button className='flex gap-8 bg-[#A06ECE] text-white rounded-[12px] p-5 w-full'>
+        <ArrowLeft />
         Beranda
-        </button>
-        <button className='border-2 border-[#7126B5] rounded-[16px] w-[96px] h-[32px] flex items-center p-4'>
-            <div className='flex gap-1'>
-                 <Filter color="#8A8A8A" strokeWidth={2} absoluteStrokeWidth />Filter
-            </div>
-        </button>
-        <button>
-        <Search size={32} color="#7126B5" strokeWidth={3} absoluteStrokeWidth />
-        </button>
-  </div>
-  )
-}
+      </button>
+      
+      <button className='border-2 border-[#7126B5] rounded-[16px] w-[96px] h-[32px] flex items-center justify-center p-4'>
+        <div className='flex gap-1'>
+          <Filter color="#8A8A8A" strokeWidth={2} />
+          Filter
+        </div>
+      </button>
+      
+      <button onClick={handleSearchClick} className="w-full sm:w-auto flex justify-center">
+        <Search size={32} color="#7126B5" strokeWidth={3} />
+      </button>
 
-export default ButtonHeader
+      <SearchFlightModal
+        isOpen={isModalOpen}
+        onClose={handleModalClose}
+        onSelect={handleFlightNumberSelect}
+      />
+    </div>
+  );
+};
+
+export default ButtonHeader;
